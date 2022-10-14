cf3e440ead5c5660202599d92e7f01d3ec1da71f
Bastiaan te Veldhuis <bve@netcompany.com>
2022-10-14 16:28:07 +0200

    a

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpacesPipe
+    AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent
   ],
   imports: [
     BrowserModule, FormsModule,
